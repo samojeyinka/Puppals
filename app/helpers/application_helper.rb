26fbc12085e9b9ee9b1abe8d1afb757a29329fc3
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-13 08:56:54 +0200

    style: sign up and log in button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def button_style_bg_blue
+    "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md focus:shadow-outline"
+  end
+
+  def border_line
+    "block border border-black rounded-md px-4 focus:shadow-outline"
+  end
 end
