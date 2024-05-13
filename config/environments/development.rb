9af126173184b3c7ac9321d064c261db7b819d2c
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-14 01:52:29 +0200

    feat: active storage set up with cloudinary

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  #devise 
+  #devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -37,7 +37,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
