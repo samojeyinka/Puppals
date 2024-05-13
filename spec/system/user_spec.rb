7313ddf1b4cf7f7cc6599a8c085c0bdc9c4bb5b7
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-13 09:28:58 +0200

    Test passed: user signs in

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
-RSpec.describe "User" do
+RSpec.describe "User", type: :system do
+  let(:user) { create(:user)}
 
   it "sees login and sign up link on the homepage" do
     visit "/"
@@ -20,6 +21,20 @@ RSpec.describe "User" do
     fill_in "user_password_confirmation", with: "123456"
 
     click_on "Sign up"
+
+    expect(page).to have_content("You are signed in oliv@gmail.com")
+    expect(page).to have_link("Sign Out")
+  end
+
+  it "signs in" do
+    visit "/"
+
+    click_on "Login"
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_on "Log in"
 
     expect(page).to have_content("You are signed in oliv@gmail.com")
     expect(page).to have_link("Sign Out")
