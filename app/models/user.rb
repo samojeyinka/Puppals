665a10d6d95faa60b463a38ddcccbf9b2f12e61b
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-14 10:14:38 +0200

    Test(model): validate presence of email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   has_one :profile
   has_one_attached :photo
+
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
