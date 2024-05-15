5f97498650e53c0df15b5295179ed660a4fac296
samojeyinka <samuelojeyinka@gmail.com>
2024-05-15 10:46:51 -1200

    Remove profile_id and dog_id from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_15_030719) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_15_224300) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,9 +77,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_15_030719) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "profile_id"
-    t.bigint "dog_id"
-    t.index ["dog_id"], name: "index_users_on_dog_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
