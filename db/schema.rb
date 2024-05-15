4fd37e4ad018e167f91176394ce2d660781653a6
samojeyinka <samuelojeyinka@gmail.com>
2024-05-14 15:10:39 -1200

    removed the profile id from the dog attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_234539) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_15_030719) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_234539) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "dogs", force: :cascade do |t|
     t.string "name"
@@ -50,10 +51,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_234539) do
     t.string "hobby"
     t.text "bio"
     t.bigint "user_id", null: false
-    t.bigint "profile_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["profile_id"], name: "index_dogs_on_profile_id"
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
@@ -87,6 +86,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_234539) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "dogs", "profiles"
   add_foreign_key "dogs", "users"
 end
