7d99840b37d97ecc6e4fdc334ea296d790959394
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-16 17:31:03 +0200

    test(model): Dog model validation

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,5 +3,16 @@ class Dog < ApplicationRecord
     belongs_to :profile
     has_many_attached :photos
 
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: true
+    validates :breed, :gender, :size, presence: true
+    validates :bio, :age, presence: true
 end
+
+
+# it { expect(subject).to validate_presence_of(:name) }
+#     it { expect(subject).to validate_presence_of(:breed) }
+#     it { expect(subject).to validate_presence_of(:gender) }
+#     it { expect(subject).to validate_presence_of(:size) }
+#     it { expect(subject).to validate_presence_of(:age) }
+#     it { expect(subject).to validate_presence_of(:hobby) }
+#     it { expect(subject).to validate_presence_of(:bio) }
--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Dog, type: :model do
   context "association" do
     it { is_expected.to have_many_attached(:photos)}
   end
+
+  context "validation" do
+    it { expect(subject).to validate_presence_of(:name) }
+    it { expect(subject).to validate_presence_of(:breed) }
+    it { expect(subject).to validate_presence_of(:gender) }
+    it { expect(subject).to validate_presence_of(:size) }
+    it { expect(subject).to validate_presence_of(:age) }
+    it { expect(subject).to validate_presence_of(:bio) }
+  end
 end
