a715d8382b063696bf8d8f3f6d3dd0758df1ce96
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-17 11:01:02 +0200

    test passed : user can fill in some dog info

--- spec/system/dog_spec.rb
+++ spec/system/dog_spec.rb
@@ -9,6 +9,27 @@ RSpec.describe "Dog", type: :system do
       visit "/dogs/new"
 
       expect(page).to have_content("Tell us about your pet")
+
+      fill_in "name", with: "dogy"
+      fill_in "breed", with: "akita"
+      fill_in "gender", with: "male"
+      # fill_in "size", with: "large"
+      # fill_in "age", with: 12
+      # fill_in "bio", with: "A good hygiene dog"
     end
   end
 end
+
+# create_table "dogs", force: :cascade do |t|
+#   t.string "name"
+#   t.string "breed"
+#   t.string "gender"
+#   t.integer "size"
+#   t.integer "age"
+#   t.string "hobby"
+#   t.text "bio"
+#   t.bigint "user_id", null: false
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   t.index ["user_id"], name: "index_dogs_on_user_id"
+# end
