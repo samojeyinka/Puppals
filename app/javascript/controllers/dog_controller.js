62e1503f59925f45df1c3c5f40f68fc594786546
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-18 09:52:40 +0200

    Single Selection Drop Down for Single Option

--- app/javascript/controllers/dog_controller.js
+++ app/javascript/controllers/dog_controller.js
@@ -1,43 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 import SlimSelect from 'slim-select'
-import 'slim-select/dist/slimselect.css'
+// import 'slim-select/dist/slimselect.css'
 
 // Connects to data-controller="dog"
 export default class extends Controller {
-  static targets = ["selectGender", "gender", "size", "selectSize", "age", "selectAge", "hobby", "selectHobby"]
 
   connect() {
     new SlimSelect({
       select: this.element
     })
   }
-
-  selectGender() {
-    const selectedGender = this.selectGenderTarget.value
-    this.genderTarget.value = selectedGender
-    this.selectGenderTarget.value = ""
-  }
-
-  selectSize() {
-    const selectedSize = this.selectSizeTarget.value
-    this.sizeTarget.value = selectedSize
-    this.selectSizeTarget.value = ""
-  }
-
-  selectAge() {
-    const selectedAge = this.selectAgeTarget.value
-    this.ageTarget.value = selectedAge
-    this.selectAgeTarget.value = ""
-  }
-
-  // selectHobby() {
-  //   const selectedHobby = this.selectHobbyTarget.value
-  //   this.hobbyTarget.value = selectedHobby
-  //   this.selectHobbyTarget.value = ""
-
-  //   // const selectedHobbies = Array.from(this.selectHobbyTarget.selectedOptions).map(option => option.value);
-
-  //   // console.log(selectedHobbies)
-  // }
-
 }
