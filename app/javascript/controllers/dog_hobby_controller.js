a8adde5521fa0cfd7aec40bd21c8f9691b025987
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-18 11:40:37 +0200

    chore: changed placeholder to an empty string

--- app/javascript/controllers/dog_hobby_controller.js
+++ app/javascript/controllers/dog_hobby_controller.js
@@ -10,7 +10,7 @@ export default class extends Controller {
         closeOnSelect: false,
       },
       settings: {
-        placeholderText: 'Hobby*',
+        placeholderText: '',
       },
     })
   }
