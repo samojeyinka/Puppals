8891dd7c04ddfcb9d06de63a8d1c110f4d443579
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-18 12:19:54 +0200

    Remove comment

--- app/javascript/controllers/dog_controller.js
+++ app/javascript/controllers/dog_controller.js
@@ -1,8 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import SlimSelect from 'slim-select'
-// import 'slim-select/dist/slimselect.css'
 
-// Connects to data-controller="dog"
 export default class extends Controller {
 
   connect() {
