e85e631a5e8d10086ce010e14d5cfcf5a414075a
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-19 11:19:58 +0200

    Removed Attached Avatar From Dog Model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -5,10 +5,6 @@ class Dog < ApplicationRecord
     has_one_attached :photo_2
     has_one_attached :photo_3
 
-    has_one_attached :avatar do |attachable|
-        attachable.variant :thumb, resize_to_limit: [100, 100]
-    end
-
     validates :name, presence: true, uniqueness: true
     validates :breed, :gender, :size, presence: true
     validates :bio, :age, presence: true
