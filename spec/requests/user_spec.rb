068f187c909fc9626728719da527e128a1e5a162
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-19 20:36:57 +0200

    test: request test for login response

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,13 +1,24 @@
 require "rails_helper"
 
 RSpec.describe "User", type: :request do
-  let(:user) { create(:user) }
+  let!(:user) { create(:user) }
 
   it "signs in user" do
-    sign_in user
+    # sign_in user  => This is helper exists for user to sign in
+    post  user_session_path, params: {
+      user: {
+        email: "oliv@gmail.com",
+        password: '123456'
+      }
+    }
 
-    get "/feeds"
-    expect(response).to have_http_status(:ok)
-    expect(response).to render_template(:index)
+    expect(response).to redirect_to("/")
+    follow_redirect!
+
+    expect(response).to redirect_to(new_dog_path)
+    follow_redirect!
+
+    expect(response).to render_template(:new)
+    expect(response.body).to include("Tell us about your pet")
   end
 end
