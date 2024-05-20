af4c2c95a9fd3bfa43641c77cbdc5eb4400f0c1d
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-21 01:26:24 +0200

    Remove Profile ID from The Dog's Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_17_210459) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_20_215859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,10 +61,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_17_210459) do
     t.text "hobby", default: [], array: true
     t.text "bio"
     t.bigint "user_id", null: false
-    t.bigint "profile_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["profile_id"], name: "index_dogs_on_profile_id"
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
@@ -116,7 +114,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_17_210459) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
-  add_foreign_key "dogs", "profiles"
   add_foreign_key "dogs", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
