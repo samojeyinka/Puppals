c5a8898122e56e82a86056fd73689e53a742a838
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-21 10:32:38 +0200

    User Profile Page is Shown Upon clicking Continue on the Dog Profile Form Input Page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,10 @@
 class ProfilesController < ApplicationController
     def show
-            @me = User.find(params[:id]);
-            @profile = Profile.find(params[:id])
-        
+        @me = User.find(params[:id]);
+        @profile = Profile.find(params[:id])
+    end
 
+    def new
+        @profile = Profile.new
     end
 end
