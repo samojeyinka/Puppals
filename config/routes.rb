2e5724aae9fc8381b7821fe6e4dc08f2405d2867
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-25 12:11:07 +0200

    User Redirected to the Profile Page Upon Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+
+  devise_for :users, controllers: {
+    sessions: "sessions"
+  }
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
 
   resources :feeds, only: %i(index)
   resources :dogs, only: %i(new create)
