807fe9602c5e5c751354cf3932dd7b345534d565
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-26 23:17:24 +0200

    test(request): user registers/logins and redirect

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,24 +1,41 @@
 require "rails_helper"
 
 RSpec.describe "User", type: :request do
-  let!(:user) { create(:user) }
 
-  it "signs in user" do
-    # sign_in user  => This is helper exists for user to sign in
-    post  user_session_path, params: {
+
+  it "signs in the user redirect to the feeds page" do
+    user = create(:user, email: "olive11@gmail.com")
+    # sign_in user  #=> This is helper exists for user to sign in
+    post user_session_path, params: {
       user: {
-        email: "oliv@gmail.com",
+        email: "olive11@gmail.com",
         password: '123456'
       }
     }
 
-    expect(response).to redirect_to("/")
+    expect(response).to redirect_to(feeds_path)
     follow_redirect!
 
-    expect(response).to redirect_to(new_dog_path)
+    expect(response).to render_template(:index)
+    expect(response.body).to include("hello. This is feeds")
+    expect(response.body).to include("You are signed in #{user.email}")
+  end
+
+
+  it "registers user and redirect to the dogs profile page" do
+    post "/users", params: {
+      user: {
+        email: "olive1@gmail.com",
+        password: '123456',
+        password_confirmation: "123456"
+      }
+    }
+
+    expect(response).to redirect_to(root_path)
     follow_redirect!
 
-    expect(response).to render_template(:new)
+    expect(response).to redirect_to(profile_path)
+    follow_redirect!
     expect(response.body).to include("Tell us about your pet")
   end
 end
