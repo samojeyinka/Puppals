57272fff3ac7cbe8df0ba08fde82060394c3ee4e
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-06-01 23:32:16 +0200

    test: registered user creates a dog profile

--- spec/system/dog_spec.rb
+++ spec/system/dog_spec.rb
@@ -1,55 +1,62 @@
 require "rails_helper"
 
 RSpec.describe "Dog", type: :system do
-  let(:user) { create(:user)}
-  let(:dogs) { create(:dog, user: user) }
+  # let(:user) { create(:user)}
+  # let(:dogs) { create(:dog, user: user) }
 
 
   # let(:user) { create(:user, email: "scoal@gmail.com") }
   # let!(:photo_1) { fixture_file_upload(file_fixture('images.jpg')) }
-  # context "when user registers/logins" do
-  #   it "creates dog profile" do
-  #     sign_in(user)
+  context "when user registers" do
+    it "creates dog profile" do
+      visit "/"
 
-  #     visit "/dogs/new"
+      click_on "Register"
 
-  #     expect(page).to have_content("Tell us about your pet")
-  #     # dogs.photo_1
-  #     # fixture_file_upload(file_fixture('images.jpg'))
+      fill_in "Email", with: "oliv1@gmail.com"
+      fill_in "user_password", with: "123456"
+      fill_in "user_password_confirmation", with: "123456"
 
-  #     fill_in "dog[name]", with: "dogy"
-  #     fill_in "dog[breed]", with: "akita"
+      click_on "Sign up"
 
-  #     within('#gender') do
-  #       expect(page).to have_selector('label', text: 'Gender*')
-  #       find('div.ss-main').click
-  #     end
-  #     find('.ss-list', text: 'Male').click
+      expect(page).to have_content("Tell us about your pet")
 
-  #     within('#size') do
-  #       expect(page).to have_selector('label', text: 'Size*')
-  #       find('div.ss-main').click
-  #     end
-  #     find('.ss-list', text: 'Large').click
+      fill_in "dog[name]", with: "dogy"
+      fill_in "dog[breed]", with: "akita"
 
-  #     within('#age') do
-  #       expect(page).to have_selector('label', text: 'Age*')
-  #       find('div.ss-main').click
-  #     end
-  #     find('.ss-list', text: '1 yr').click
+      within('#gender') do
+        expect(page).to have_selector('label', text: 'Gender*')
+        expect(page).to have_css('.ss-main', visible: true)
+        find('.ss-main', wait: 10).click
+      end
+      find('.ss-list', text: 'Male').click
 
-  #     within('#hobby') do
-  #       expect(page).to have_selector('label', text: 'Hobby*')
-  #       find('div.ss-main').click
-  #     end
-  #     find('.ss-list', text: 'Hiking').click
+      within('#size') do
+        expect(page).to have_selector('label', text: 'Size*')
+        find('div.ss-main').click
+      end
+      find('.ss-list', text: 'Large').click
 
-  #     fill_in "dog[bio]", with: "A good hygiene dog"
+      within('#age') do
+        expect(page).to have_selector('label', text: 'Age*')
+        find('div.ss-main').click
+      end
+      find('.ss-list', text: '1 yr').click
 
-  #     click_on "Continue"
-  #     expect(page).to have_content("This is user profile")
-  #   end
-  # end
+      within('#hobby') do
+        expect(page).to have_selector('label', text: 'Hobby*')
+        find('div.ss-main').click
+      end
+      find('.ss-list', text: 'Hiking').click
+
+      fill_in "dog[bio]", with: "A good hygiene dog"
+
+      attach_file('dog[photo_1]', 'app/assets/images/images.jpg', visible: false)
+
+      click_on "Continue"
+      expect(page).to have_content("This is user profile")
+    end
+  end
 
   # fcontext "when user create second dog profile" do
   #   let!(:dog) { create(:dog, user: user) }
