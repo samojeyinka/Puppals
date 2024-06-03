0e4a109af5ec35ef4a83bc658ca5817532bce667
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-06-03 02:53:56 +0200

    test: user can creates dog and user profile

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,15 @@
 FactoryBot.define do
   factory :user do
-    email { "oliv@gmail.com" }
+    email { "sepe@gmail.com" }
     password { "123456" }
+    password_confirmation { "123456" }
+  end
+
+  trait :logged_in do
+    email { "olive@gmail.com" }
+    password { "123456" }
+  end
+
+  trait :registered do
   end
 end
--- spec/system/dog_spec.rb
+++ spec/system/dog_spec.rb
@@ -1,13 +1,86 @@
 require "rails_helper"
 
 RSpec.describe "Dog", type: :system do
-  # let(:user) { create(:user)}
-  # let(:dogs) { create(:dog, user: user) }
+  # context "when user registers and has created do profile" do
+  #   # let!(:user_registration) { create(:user, :registered)}
+  #   # let!(:dog) { create(:dog, name: "Puppy", user: user_registration) }
+  #   let!(:user) { create(:user, :registered)}
 
+  #   before do
+  #     login_as(user, scope: :user)
+  #     visit profile_path
+  #   end
+  #   it "creates user profile" do
+  #     expect(page).to have_content("This is user profile.")
+  #   end
+  # end
 
-  # let(:user) { create(:user, email: "scoal@gmail.com") }
-  # let!(:photo_1) { fixture_file_upload(file_fixture('images.jpg')) }
   context "when user registers" do
+    fit "creates user_profile" do
+      visit "/"
+
+      click_on "Register"
+
+      fill_in "Email", with: "oliv1@gmail.com"
+      fill_in "user_password", with: "123456"
+      fill_in "user_password_confirmation", with: "123456"
+
+      click_on "Sign up"
+
+      expect(page).to have_content("Tell us about your pet")
+
+      fill_in "dog[name]", with: "dogy"
+      fill_in "dog[breed]", with: "akita"
+
+      within('#gender') do
+        expect(page).to have_selector('label', text: 'Gender*')
+        expect(page).to have_css('.ss-main', visible: true)
+        find('.ss-main', wait: 10).click
+      end
+      find('.ss-list', text: 'Male').click
+
+      within('#size') do
+        expect(page).to have_selector('label', text: 'Size*')
+        find('div.ss-main').click
+      end
+      find('.ss-list', text: 'Large').click
+
+      within('#age') do
+        expect(page).to have_selector('label', text: 'Age*')
+        find('div.ss-main').click
+      end
+      find('.ss-list', text: '1 yr').click
+
+      within('#hobby') do
+        expect(page).to have_selector('label', text: 'Hobby*')
+        find('div.ss-main').click
+      end
+      find('.ss-list', text: 'Hiking').click
+
+      fill_in "dog[bio]", with: "A good hygiene dog"
+
+      attach_file('dog[photo_1]', 'app/assets/images/images.jpg', visible: false)
+
+      click_on "Continue"
+
+      expect(page).to have_content("This is user profile")
+      fill_in "profile[first_name]", with: "Scoal"
+      fill_in "profile[last_name]", with: "Scoalee"
+      fill_in "profile[bio]", with: "I am tall and upright"
+
+      within('#country') do
+        expect(page).to have_selector('label', text: 'Country*')
+        find('div.ss-main').click
+      end
+      find('.ss-list', text: 'Angola').click
+
+      fill_in "profile[city]", with: "Copenhagen"
+      attach_file('profile[photo]', 'app/assets/images/images.jpg', visible: false)
+
+      click_on "Continue"
+      expect(page).to have_content("Your profile oliv1@gmail.com")
+    end
+
     it "creates dog profile" do
       visit "/"
 
