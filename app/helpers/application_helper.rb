a15bd56be1ab3a6e7dad1fb71b1ce469881e5711
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-06-04 09:21:12 +0200

    Mobile Navigation Bar excluded from The User Profile Input Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,11 @@ module ApplicationHelper
     "block border border-black rounded-md px-4 py-1 focus:shadow-outline"
   end
 
-  def profile_input_page?
-    controller_name == "dogs" && action_name == "new" || action_name == "create"
+  def dog_profile_input?
+    controller_name == "dogs" && (action_name == "new" || action_name == "create")
+  end
+
+  def user_profile_input?
+    controller_name == "user_profiles" && (action_name == "new" || action_name == "create")
   end
 end
